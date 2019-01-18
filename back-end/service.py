f00c751b50189f220ee754decdd7c85d3d4b1ed7
Alex Martens <alex.smartens@gmail.com>
2019-01-18 14:39:28 -0500

    Added back end

---
+++ back-end/service.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+
+
+## Part 0: loading legs, stops and driver location data
+f = open(".\data\legs.json")
+legs = json.load(f)
+f.close()
+
+f = open(".\data\stops.json")
+stops = json.load(f)
+f.close()
+
+f = open(".\data\driver_location.json")
+driver_location = json.load(f)
+f.close()
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+## Part 1: adding a REST API to retrieve the given list of stops and legs from server
+# GET list of legs
+@app.route("/legs", methods=["GET"])
+def get_legs():
+    return jsonify({
+                        "legs": legs
+                    })
+
+# GET list of legs
+@app.route("/stops", methods=["GET"])
+def get_stops():
+    return jsonify({
+                        "stops": stops
+                    })
+
+
+## Part 3: adding a REST API to get the driver’s current position
+# GET driver’s current position
+@app.route("/driver", methods=["GET"])
+def get_driver_location():
+    return jsonify({
+                        "Driver’s current position": driver_location
+                    })
+
+
+## Part 7: adding a REST API to update the driver’s current position
+# PUT driver’s current position
+@app.route("/driver", methods=["PUT"])
+def put_driver_location():
+    provided_driver_location = request.json["Driver’s current position"]
+    globals()["driver_location"] = provided_driver_location
+
+    return jsonify({
+        "Driver’s current position": driver_location
+    })
+
+
+
+
+
+if __name__=="__main__":
+    # Initial data visualization
+
+    app.run(debug=True, port=8080)
